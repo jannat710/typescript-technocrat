989636c719c95b6b059f8758539a8086822c2fe6
e <jannatul0040@gmail.com>
2024-11-05 12:21:24 +0600

    inheritance

---
+++ module3/3.2.ts
@@ -0,0 +1,50 @@
+{
+    // oop-inheritance
+    class Person{
+        name: string;
+        age: number;
+        address: string;
+
+        constructor(name: string, age: number, address: string) {
+            this.name = name;
+            this.age = age;
+            this.address = address;
+        }
+        // Method
+        getSleep(numOfHours: number) {
+            console.log(`${this.name} will sleep for ${numOfHours}`);
+        }
+    }
+
+
+
+
+
+    class Student extends Person{
+        constructor(name: string, age: number, address: string) {
+            super(name,age,address)
+        }
+        
+    }
+    const student1 = new Student("Mr. student", 20, "USA");
+    student1.
+
+
+
+    class Teacher extends Person{
+        designation:string;
+
+        constructor(name: string, age: number, address: string,designation:string) {
+            super(name,age,address)
+            this.designation=designation;
+        }
+        // Method
+        
+        takeClass(numOfClass:number){
+            console.log(`${this.name} will take ${numOfClass}`); 
+        }
+    }
+    const teacher = new Teacher("Mr. student", 20, "USA","Professor");
+    teacher.
+    // 
+}
